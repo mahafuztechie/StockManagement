02d37bc3ce194c32ef420bf68e0f0ebb82a231a7
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-02-02 20:05:35 +0530

    [mahafuz] comments & modified

--- StockManagement/StockManagement/Program.cs
+++ StockManagement/StockManagement/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using StockManagement;
 
+//path for json file
 const string STOCK_JSON = @"C:\Users\dell\source\repos\StockManagement\StockManagement\StockManagement\StockManagement\Stock.json";
+
 StockManager stockmanager = new StockManager();
 stockmanager.StockDataReport(STOCK_JSON);
